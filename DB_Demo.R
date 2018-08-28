6200cf54e7853b0347ba029d0a373f00f539e3af
Nate Jones <njones@rstudio03.research.sesync.org>
2018-08-28 17:19:41 -0400

    Initial Checks

--- DB_Demo.R
+++ DB_Demo.R
@@ -111,17 +111,23 @@ db_get_water_level_ts_function<-function(site_code){
   
   #Retreive Sampling Feature ID for site code
   SamplingFeatureID<-RSQLite::dbGetQuery(db,
-                                        "SELECT SamplingFeatureID FROM SamplingFeatures WHERE SamplingFeatureCode = :x", 
+                                        "SELECT SamplingFeatureID 
+                                          FROM SamplingFeatures 
+                                          WHERE SamplingFeatureCode = :x", 
                                         params=list(x=site_code))
   
   #Retreive Feature Action ID[s] for site code
   FeatureActionID<-RSQLite::dbGetQuery(db,
-                                       "SELECT FeatureActionID FROM FeatureActions WHERE SamplingFeatureID = :x", 
-                                         params=list(x=SamplingFeatureID[,1]))
+                                       "SELECT FeatureActionID 
+                                          FROM FeatureActions 
+                                          WHERE SamplingFeatureID = :x", 
+                                       params=list(x=SamplingFeatureID[,1]))
   
   #Retreive Result ID[s] for each feature action
   ResultID<-RSQLite::dbGetQuery(db,
-                                  "SELECT ResultID FROM Results WHERE FeatureActionID = :x", 
+                                  "SELECT ResultID 
+                                      FROM Results 
+                                      WHERE FeatureActionID = :x", 
                                    params=list(x=FeatureActionID[,1]))
   
   #Retreive Result values
