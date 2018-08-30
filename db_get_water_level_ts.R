9ef84ab66703fbe55b521edb44566797dc53c556
Nate Jones <njones@rstudio03.research.sesync.org>
2018-08-29 22:33:56 -0400

    ts upload (Try #1)

--- db_get_water_level_ts.R
+++ db_get_water_level_ts.R
@@ -5,28 +5,28 @@ db_get_water_level_ts<-function(site_code){
   SamplingFeatureID<-RSQLite::dbGetQuery(db,
                                          "SELECT SamplingFeatureID FROM SamplingFeatures WHERE SamplingFeatureCode = :x", 
                                          params=list(x=site_code))
-  
-  #Retreive Feature Action ID[s] for site code
-  FeatureActionID<-RSQLite::dbGetQuery(db,
-                                       "SELECT FeatureActionID FROM FeatureActions WHERE SamplingFeatureID = :x", 
-                                       params=list(x=SamplingFeatureID[,1]))
-  
-  #Retreive Result ID[s] for each feature action
-  ResultID<-RSQLite::dbGetQuery(db,
-                                "SELECT ResultID FROM Results WHERE FeatureActionID = :x", 
-                                params=list(x=FeatureActionID[,1]))
-  
-  #Retreive Result values
-  Values<-RSQLite::dbGetQuery(db,
-                              "SELECT ValueDateTime, DataValue
-                                    FROM TimeSeriesResultValues
-                                    WHERE ResultID = :x", 
-                              params=list(x=ResultID[,1]))
-  
-  #Clean up values df
-  Values$ValueDateTime<-as.POSIXct(Values$ValueDateTime)
-  colnames(Values)<-c("Timestamp", "water_level_m")
-  
-  #Export Values
-  Values
+  if(nrow(SamplingFeatureID)>0){ 
+    #Retreive Feature Action ID[s] for site code
+    FeatureActionID<-RSQLite::dbGetQuery(db,
+                                         "SELECT FeatureActionID FROM FeatureActions WHERE SamplingFeatureID = :x", 
+                                         params=list(x=SamplingFeatureID[,1]))
+    if(nrow(FeatureActionID)>0){
+      #Retreive Result ID[s] for each feature action
+      ResultID<-RSQLite::dbGetQuery(db,
+                                    "SELECT ResultID FROM Results WHERE FeatureActionID = :x", 
+                                    params=list(x=FeatureActionID[,1]))
+      if(nrow(ResultID)>0){
+        #Retreive Result values
+        Values<-RSQLite::dbGetQuery(db,
+                                    "SELECT ValueDateTime, DataValue
+                                          FROM TimeSeriesResultValues
+                                          WHERE ResultID = :x", 
+                                    params=list(x=ResultID[,1]))
+        
+        #Clean up values df
+        Values$ValueDateTime<-as.POSIXct(Values$ValueDateTime)
+        colnames(Values)<-c("Timestamp", "water_level_m")
+        
+        #Export Values
+        Values}}}
 }
