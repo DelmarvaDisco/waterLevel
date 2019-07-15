72779f73e877f718a1a54bad1ed959a2f2997e66
Nate Jones <njones@rstudio03.research.sesync.org>
2019-07-15 15:47:06 -0400

    included offset

--- functions/waterDepth_fun.R
+++ functions/waterDepth_fun.R
@@ -1,4 +1,7 @@
-waterDepth_fun<-function(#from working df
+waterDepth_fun<-function(#db information
+                         db = NULL,
+                         site = site, 
+                         #from working df
                          Timestamp, 
                          waterHeight,
                          #from well log
@@ -9,30 +12,33 @@ waterDepth_fun<-function(#from working df
                          waterDepth,
                          wellHeight){
   
-  #Organize Data~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  #Organize Data~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   #Water height time series data
   ts<-tibble(Timestamp, waterHeight) %>% 
-    mutate(Timestamp = floor_date(Timestamp, "day")) %>%
+    mutate(Timestamp = date(floor_date(Timestamp, "day"))) %>%
     group_by(Timestamp) %>%
-    summarise(waterHeight = mean(waterHeight, na.rm = T))
+    summarise(waterHeight = mean(waterHeight, na.rm = T)) %>% na.omit(.)
   
   #Water depth at download (well log file)
   depths<-tibble(Timestamp = download_date, waterDepth=Relative_Water_Level_m) %>%
-    mutate(Timestamp = as.POSIXct(Timestamp))
+    mutate(Timestamp = ymd(Timestamp)) 
   
   #Survey
   survey<-tibble(Timestamp = surveyDate, waterDepth, wellHeight)
   
-  #Estimate water depth during each download~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #Estimate water depth during each download~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if(!(depths$Timestamp %in% ts$Timestamp)){
+    ts$Timestamp[ts$Timestamp==max(ts$Timestamp)]<-depths$Timestamp[1]
+  }
   depths<-left_join(depths, ts) %>%
     mutate(offset = waterDepth - waterHeight, 
            event = "download") %>%
     select(offset, event) %>% na.omit()
-  
-  
+
   #Make estimates from survey
   survey<- survey %>%
-    mutate(Timestamp = mdy(Timestamp, tz='GMT')) %>% 
+    mutate(Timestamp = mdy(Timestamp)) %>% 
     left_join(., ts, by='Timestamp') %>%
     mutate(survey_wetland_well = waterDepth -waterHeight,
            survey_upland_well  = wellHeight) %>%
@@ -43,6 +49,21 @@ waterDepth_fun<-function(#from working df
   #bind rows
   output<-bind_rows(depths, survey)
     
-  #Export output
+  #Query offset from db [if there is an offset in the db]~~~~~~~~~~~~~~~~~~~~~~~
+  diff<-tryCatch(
+    db_get_ts(db, 
+              site, 
+              variable_code_CV = 'offset', 
+              mdy("1/1/1900"), mdy('1/1/3000')), 
+    error = function(e) 0)
+  if(diff!=0){
+    diff<-unique(diff$offset)
+    diff<-tibble(event = "offset", 
+                 offset = diff)
+    output<-bind_rows(diff, output)
+  }
+  
+  #Export to global environment~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   output
+  
 }
