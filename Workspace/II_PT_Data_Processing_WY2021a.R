59fd85da46d2157649618ba4fb67fdcebcd657f4
James Maze <jtmaze@vt.edu>
2022-03-15 10:44:08 -0400

    Field sheet good to go

--- Workspace/II_PT_Data_Processing_WY2021a.R
+++ Workspace/II_PT_Data_Processing_WY2021a.R
@@ -7,6 +7,12 @@
 
 #Notes on this download
 
+#  - DB-SW serial number did not read in when using the download function. 
+#    The column name was weird ("Abs Pres, kPa (LGR S/N: 10258771, SEN S/N: 10258771, LBL: PSI)")
+#    Since the serial number had LBL : PSI after it, download_fun broke. Manually assigned SN to the site
+#    in Step #2
+#  - 
+
 #Steps
 # Step 1: Organize workspace
 # Step 2: Field Worksheet
@@ -40,17 +46,42 @@ source("functions//db_get_ts.R")
 source("functions//fun_anomalous.R")
 
 #data directory
-data_dir <- "data//20210525_Downloads"
+data_dir <- "data//20210525_Downloads//"
 
 #list pt, baro, and log file locations
 pt_files<-list.files(paste0(data_dir, "export"), full.names =  TRUE) 
 pt_files<-pt_files[!str_detect(pt_files, "log")]
 pt_files<-pt_files[!str_detect(pt_files, "Baro")]
-field_logs<-paste0(data_dir, 'well_log.csv')
+
+#gather pt data
+df<-pt_files %>% map_dfr(download_fun) 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Step 2: Field worksheets -------------------------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Download Field Worksheet
+field_logs <- read_csv(paste0(data_dir, "well_log.csv")) 
+
+
+#Check to make sure pt files match field logs
+check_fun(pt_files, field_logs)
+
+rm(check_fun_errors)
+
+#create df of site name, sonde_id, and measured water level & Notes
+field_logs<-field_logs %>% 
+  select(Site_Name, Sonde_ID, Relative_water_level_m, Notes)
+
+#join to master df make Sonde_ID a character
+df$Sonde_ID <- as.character(df$Sonde_ID)
+field_logs$Sonde_ID <- as.character(field_logs$Sonde_ID)
+
+dt<-df %>% left_join(., field_logs)
+ 
+#!!! download_fun didn't recognize DB-SW's SN. Wonky column name. Manual fix here. 
+dt <- dt %>% 
+  mutate(Sonde_ID = if_else(is.na(Sonde_ID), "10258771", Sonde_ID)) %>% 
+  mutate(Site_Name = if_else(is.na(Site_Name), "DB-SW", Site_Name))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Step 3: Well offsets -------------------------------------------------------
